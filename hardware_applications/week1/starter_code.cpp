6ca819a476036f6c7b03d8fd567920ff041bdaac
Billy Vaughn <vaughnbilly8@gmail.com>
2017-09-28 18:47:42 -0400

    Added an example file and addedcode to starter code

--- hardware_applications/week1/starter_code.cpp
+++ hardware_applications/week1/starter_code.cpp
@@ -3,3 +3,22 @@
 
 using namespace std;
 
+int main() {
+    RJRobot robot;
+
+    cout << "Robot is ready." << endl;
+
+    robot.SetMotor(B, 200);
+
+    for (int i = 0; i < 4; i++) {
+        robot.SetMotor(A, 200);
+        robot.Wait(1000ms);
+        robot.SetMotor(A, -200);
+        robot.Wait(1300ms);
+    }
+
+    robot.StopMotors();
+
+    return 0;
+
+}
